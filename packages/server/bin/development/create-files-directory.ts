c92e616774fdc97d8270eb9cf312c26182877c10
William Augusto <william.santos315@outlook.com>
2021-12-26 12:06:38 -0300

    :green_heart: ci(base): workflow de verificacoes apos push

--- packages/server/bin/development/create-files-directory.ts
+++ packages/server/bin/development/create-files-directory.ts
@@ -1,13 +1,16 @@
 import { green, yellow } from 'chalk';
-import { mkdirSync, rmSync } from 'fs';
+import { existsSync, mkdirSync, rmSync } from 'fs';
 
 export default async function createPublicDirectory(): Promise<void> {
   const directory = `${__dirname}/../../public`;
 
-  console.log(`[${yellow('DEVELOPMENT')}] ${green('files')} directory was created!`);
+  if (existsSync(directory)) {
+    rmSync(directory, { recursive: true });
+  }
 
-  rmSync(directory, { recursive: true });
   mkdirSync(directory, { recursive: true });
   mkdirSync(`${directory}/images`, { recursive: true });
   mkdirSync(`${directory}/images/accounts`, { recursive: true });
+
+  console.log(`[${yellow('DEVELOPMENT')}] ${green('files')} directory was created!`);
 }
