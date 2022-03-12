66a303e95da081dd7da869b9af35a25745110e52
William Augusto <william.santos315@outlook.com>
2022-03-12 20:01:12 -0300

    :recycle: refactor(client): login component

--- packages/client/src/app/components/header/dialog-login/dialog-login.component.ts
+++ packages/client/src/app/components/header/dialog-login/dialog-login.component.ts
@@ -1,9 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Observer } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 
-import { Account, Login, LoginResponse } from '@interfaces/index';
+import { Account, Login } from '@interfaces/index';
 import { TokenService } from '@core/shared/token/token.service';
 import { SnackBarService } from '@core/shared/snack-bar/snack-bar.service';
 import { DialogLoginService } from './services/dialog-login.service';
@@ -14,61 +14,53 @@ import { DialogLoginService } from './services/dialog-login.service';
 })
 export class DialogLoginComponent {
   public loginForm: FormGroup;
-  public hide: boolean = true;
-  public sendingForm: boolean = false;
-  public emailOrPasswordIncorrect: boolean = false;
+  public sendingForm: boolean;
+  public hidePassword: boolean;
+  public emailOrPasswordIsIncorrect: boolean;
 
   public constructor(
     @Inject(MAT_DIALOG_DATA) public data: Account,
     private formBuilder: FormBuilder,
-    private loginService: DialogLoginService,
     private tokenService: TokenService,
+    private loginService: DialogLoginService,
     private snackBarService: SnackBarService,
     private dialogRef: MatDialogRef<DialogLoginComponent>,
   ) {
+    this.hidePassword = true;
+    this.sendingForm = false;
+    this.emailOrPasswordIsIncorrect = false;
     this.loginForm = this.formBuilder.group({
       email: [data.email, [Validators.required, Validators.email]],
       password: [null, [Validators.required]],
     });
   }
 
-  public sendLoginFormByEnterKey(): void {
-    if (this.loginForm.valid) {
-      this.sendForm();
-    }
-  }
-
-  public sendForm(): void {
+  public async sendForm(): Promise<void> {
     this.sendingForm = true;
-    this.emailOrPasswordIncorrect = false;
+    this.emailOrPasswordIsIncorrect = false;
 
-    const loginForm = this.loginForm.getRawValue() as Login;
-    this.loginService.login(loginForm).subscribe(this.loginSubscribeActions());
-  }
+    try {
+      const loginForm = this.loginForm.getRawValue() as Login;
+      const data = await lastValueFrom(this.loginService.login(loginForm));
 
-  private loginSubscribeActions(): Partial<Observer<any>> {
-    return {
-      next: (data: LoginResponse): void => {
-        this.tokenService.setToken(data.token);
-        this.dialogRef.close(true);
-        this.snackBarService.open('Welcome back!', '<3', 4000);
-      },
-      error: (err: any): void => {
-        this.sendingForm = false;
+      this.dialogRef.close(true);
+      this.tokenService.setToken(data.token);
+      this.snackBarService.open('Welcome back!', '<3', 4000);
+    } catch (err: any) {
+      this.sendingForm = false;
 
-        if (JSON.stringify(err).includes('EMAIL_OR_PASSWORD_INCORRECT')) {
-          this.emailOrPasswordIncorrect = true;
-          return;
-        }
+      if (JSON.stringify(err).includes('EMAIL_OR_PASSWORD_INCORRECT')) {
+        this.emailOrPasswordIsIncorrect = true;
+        return;
+      }
 
-        if (JSON.stringify(err).includes('BAD_USER_INPUT')) {
-          this.loginForm.get('email')?.setErrors({ email: true });
-          return;
-        }
+      if (JSON.stringify(err).includes('BAD_USER_INPUT')) {
+        this.loginForm.get('email')?.setErrors({ email: true });
+        return;
+      }
 
-        this.dialogRef.close();
-        this.snackBarService.openUnknownError();
-      },
-    };
+      this.dialogRef.close();
+      this.snackBarService.openUnknownError();
+    }
   }
 }
